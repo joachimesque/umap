859c58c87406094a987163270f4333c92eb72285
Joachim <joachim@fourbi.eu>
2025-12-15 21:20:22 +0100

    Add point pages

--- umap/galerie/urls.py
+++ umap/galerie/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
@@ -6,4 +6,5 @@ app_name = "galerie"
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("orphelines", views.OrphanView.as_view(), name="orphelines"),
+    path("point/<str:pk>", views.PointView.as_view(), name="point"),
 ]
--- umap/galerie/views.py
+++ umap/galerie/views.py
@@ -3,11 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
 from . import models
 
 class IndexView(ListView):
     model = models.LayerPoint
     paginate_by = 100
+    ordering = "-date"
 
 
 class OrphanView(ListView):
@@ -15,3 +18,7 @@ class OrphanView(ListView):
 
     def get_queryset(self):
         return models.Picture.objects.filter(layer_point__isnull=True).order_by('-datetime')
+
+
+class PointView(DetailView):
+    model = models.LayerPoint

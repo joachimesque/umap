5c0f08ca4de5066c8aa0a9ef11b9b312d6542cd4
Joachim <joachim@fourbi.eu>
2025-12-16 19:16:01 +0100

    Better color getter

--- umap/galerie/utils.py
+++ umap/galerie/utils.py
@@ -20,58 +20,75 @@ THUMBMAPS_DIR = "thumbmap"
 #     max_zoom=20,
 # )
 
-def get_color(type, layer_pk):
-    color = "#00008B"
-    if (type == "dessin"):
-        color = "#00BFFF"
-    if (type == "peinture"):
-        color = "#1E90FF"
 
-    if (layer_pk == "bfaa6d1f-9bf3-4e28-9bfd-cc834e95ccd2"): # EFFACÉS
-        color = "#2F4F4F"
-    if (layer_pk == "1940693e-8252-4b8e-ac35-e83a9eaa1823"): # A CONFIRMER
-        color = "#800080"
-    if (layer_pk == "7fe1c626-38df-4ed8-9446-a0e36aacce5f"): # À PHOTOGRAPHIER
-        color = "#CD5C5C"
+def apply_rules(layer_point, default, ruleset):
+    if ruleset == "map":
+        settings = layer_point.map.settings
+    if ruleset == "layer":
+        settings = layer_point.layer.settings
+
+    if "rules" in settings:
+        for rule in settings["rules"]:
+            if "color" in rule["properties"]:
+                cond_key, cond_value = rule["condition"].split("=")
+                if (
+                    layer_point.json_data["properties"].get(cond_key, None)
+                    == cond_value
+                ):
+                    return rule["properties"]["color"]
+
+    return default
+
+
+def get_color(layer_point):
+    color = "#00008B"
+
+    color = apply_rules(layer_point, color, "map")
+
+    if "color" in layer_point.layer.settings:
+        color = layer_point.layer.settings["color"]
+
+    color = apply_rules(layer_point, color, "layer")
+
+    if "color" in layer_point.json_data["properties"].get("_umap_options", {}):
+        color = layer_point.json_data["properties"]["_umap_options"]["color"]
 
     return color
 
 
 POINT_TYPES = {
-    "crayon": "✏️",
+    "dessin": "✏️",
     "peinture": "🎨",
 }
 
 
-OLD_MARKER = r'(<g clip-path=\"url\(#page\)\" transform=\"translate\([\d\.\-]+, 0\)\"><path d=\"M 100[\d\. la\-Z]+\" fill=\"#ff0000\" opacity=\"1.0\" stroke=\"#ffffff\" stroke-width=\"1\" \/><\/g>){3}'
+OLD_MARKER = r"(<g clip-path=\"url\(#page\)\" transform=\"translate\([\d\.\-]+, 0\)\"><path d=\"M 100[\d\. la\-Z]+\" fill=\"#ff0000\" opacity=\"1.0\" stroke=\"#ffffff\" stroke-width=\"1\" \/><\/g>){3}"
+
 
 def get_or_generate_thumbmap(layer_point, force=False):
+    if not layer_point.json_data:
+        return None
+
     point_filename = f"{layer_point.pk}.svg"
     point_path = os.path.join(THUMBMAPS_DIR, point_filename)
     point_url = default_storage.url(point_path)
-    
+
     if default_storage.exists(point_path) and not force:
         return point_url
 
     if force:
         default_storage.delete(point_path)
 
-    if not layer_point.json_data:
-        return None
-
     context = staticmaps.Context()
     context.set_tile_provider(staticmaps.tile_provider_CartoNoLabels)
     # context.set_tile_provider(TILES)
 
     point_type = layer_point.json_data["properties"].get("type", None)
-    color = get_color(
-        point_type,
-        str(layer_point.layer.pk) if layer_point.layer else None
-    )
-    new_marker = f'''<g transform="translate({(200 - 32) / 2} {(200 + 16) / 2 - 40})">
-            <path fill="{color}" d="M28 0a4 4 90 0 1 4 4v24a4 4 90 0 1-4 4h-4l-8 8-8-8H4a4 4 90 0 1-4-4V4a4 4 90 0 1 4-4Z"/>
+    point_color = get_color(layer_point)
+    new_marker = f"""<g transform="translate({(200 - 32) / 2} {(200 + 16) / 2 - 40})">
+            <path fill="{point_color}" d="M28 0a4 4 90 0 1 4 4v24a4 4 90 0 1-4 4h-4l-8 8-8-8H4a4 4 90 0 1-4-4V4a4 4 90 0 1 4-4Z"/>
             <text font-size="20" text-anchor="middle" x="16" y="24" fill="white">{POINT_TYPES.get(point_type, "●")}</text>
-        </g>'''
+        </g>"""
     coords = layer_point.json_data["geometry"]["coordinates"]
     pt = staticmaps.create_latlng(float(coords[1]), float(coords[0]))
     context.add_object(staticmaps.Marker(pt, size=16))

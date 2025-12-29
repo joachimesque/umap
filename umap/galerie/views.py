bbb60b9e91bb638d74a444eabfe4ed095bbd2162
Joachim <joachim@fourbi.eu>
2025-12-29 11:54:26 +0100

    Add stort options for points

--- umap/galerie/views.py
+++ umap/galerie/views.py
@@ -13,15 +13,30 @@ class IndexView(ListView):
     paginate_by = 100
 
     def get_queryset(self):
-        queryset = (
-            models.LayerPoint.objects.annotate(last_picture=Max("picture__datetime"))
-            .order_by(
-                F("date").desc(nulls_last=True),
-                F("last_picture").desc(nulls_last=True),
-                "pk",
-            )
-            .distinct()
+        queryset = models.LayerPoint.objects.annotate(
+            last_picture=Max("picture__datetime")
+        ).order_by(
+            F("date").desc(nulls_last=True),
+            F("last_picture").desc(nulls_last=True),
+            "pk",
         )
+        if "sort" in self.request.GET:
+            if self.request.GET["sort"] == "latest_photos":
+                queryset = models.LayerPoint.objects.annotate(
+                    last_picture=Max("picture__datetime")
+                ).order_by(
+                    F("last_picture").desc(nulls_last=True),
+                    F("date").desc(nulls_last=True),
+                    "pk",
+                )
+            if self.request.GET["sort"] == "latest_uploads":
+                queryset = models.LayerPoint.objects.annotate(
+                    last_upload=Max("picture__upload_date")
+                ).order_by(
+                    F("last_upload").desc(nulls_last=True),
+                    F("date").desc(nulls_last=True),
+                    "pk",
+                )
         if "type" in self.request.GET:
             queryset = queryset.filter(type=self.request.GET["type"])
         if "date" in self.request.GET and self.request.GET["date"]:
